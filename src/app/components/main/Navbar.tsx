fde1b20b54308f2c0cd1909d527eff35b3451d32
Krutik30 <agherakrutik99@gmail.com>
2024-02-04 19:39:49 +0530

    use client or use server

---
+++ src/app/components/main/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='w-full h-[65px] fixed top-0 shodow-lg shadow-[#2A0E61]/50 bg-slate-300/30 bg-[#03001417] backdrop-blur-md  z-50 px-10'>
+        
+    </div>
+  )
+}
